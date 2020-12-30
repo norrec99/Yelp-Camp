cab004454db2fc96b54dbe71b3781c0a1b5b6e3c
Cagatay Soyer <cagataysoyer@gmail.com>
2020-12-30 18:08:01 +0300

    added delete route and delete button on show page. Basic CRUD operations completed.

--- app.js
+++ app.js
@@ -68,6 +68,12 @@ app.patch("/campgrounds/:id", async (req, res) => {
   res.redirect(`/campgrounds/${campground._id}`);
 });
 
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedCamp = await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
+
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
