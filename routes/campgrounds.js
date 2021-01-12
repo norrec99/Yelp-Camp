4a521d4646dfa26966ccb38e033d6b8de1ffcb9a
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-12 13:17:15 +0300

    campground routes restructured in its own file in the routes folder

---
+++ routes/campgrounds.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const { campgroundSchema } = require("../schemas.js");
+const ExpressError = require("../utils/ExpressError");
+const Campground = require("../models/campground");
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+router.get("/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+router.post(
+  "/",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground)
+    //   throw new ExpressError("Invalid Campground Data!", 400);
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate("reviews");
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+router.patch(
+  "/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(
+      id,
+      { ...req.body.campground },
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.delete(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const deletedCamp = await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router;
