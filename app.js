557a00b0c48ab3869723d6e7a6ef11b224bee3c9
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-20 15:15:10 +0300

    created register page and routes for it

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const password = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 const passport = require("passport");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -69,8 +70,9 @@ app.get("/fakeUser", async (req, res) => {
   res.send(newUser);
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
