352fc2e2cc09be3e01f53197e8fd21c1444e3a51
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-21 12:32:42 +0300

    added author to campground model, added my id to all campgrounds.

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "600831b3f18dec3ddaf885d2",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
