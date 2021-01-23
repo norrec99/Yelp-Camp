e76da1e17c6fba03e719c9c029ba1eef410298d2
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-23 15:53:10 +0300

    fixed seeds, hard coded coordinates for seeds, put map on right side of show page.

--- seeds/index.js
+++ seeds/index.js
@@ -29,6 +29,10 @@ const seedDB = async () => {
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed dolore beatae est natus dolores quibusdam! Error quae distinctio, ab explicabo ad, quam velit consequatur nisi alias provident, minima aut nemo.",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [-113.1331, 47.0202],
+      },
       images: [
         {
           url:
