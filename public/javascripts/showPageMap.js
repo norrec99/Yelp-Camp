9e51af3b08bf17f7ecc9731ae12e673dfc565b55
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-23 16:07:58 +0300

    added and customized map popup.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -6,4 +6,11 @@ const map = new mapboxgl.Map({
   zoom: 8, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${campground.title}</h4> <h6>${campground.location}</h6>`
+    )
+  )
+  .addTo(map);
