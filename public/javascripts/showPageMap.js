a6d917abf8302988569a6fce53a42a03f9a8ae63
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-23 15:37:26 +0300

    centered map according to its campground location, added pin to show the location.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 7, // starting zoom
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 8, // starting zoom
 });
+
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
