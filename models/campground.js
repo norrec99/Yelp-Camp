c81555e68eebd01cfb4615c11d7c7a9236e40f7d
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-25 14:49:41 +0300

    added custom popup, created virtual on schema to do custom popup.

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ const { campgroundSchema } = require("../schemas");
 const Schema = mongoose.Schema;
 const Review = require("./review");
 const { cloudinary } = require("../cloudinary");
+const opts = { toJSON: { virtuals: true } };
 
 const ImageSchema = new Schema({
   url: String,
@@ -13,33 +14,42 @@ ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
-const CampgroundSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `
+  <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+  <p>${this.location}</p>`;
 });
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
