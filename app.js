cf363db7077f81d550bdf209feedb0791e7b3bee
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-26 15:40:31 +0300

    ready to deploy.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const reviewRoutes = require("./routes/reviews");
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 // mongodb://localhost:27017/yelp-camp
 mongoose.connect(dbUrl, {
@@ -54,9 +54,11 @@ app.use(
   })
 );
 
+const secret = process.env.SECRET || "thisshouldbeabettersecret";
+
 const store = new MongoDBStore({
   url: dbUrl,
-  secret: "thisshouldbeabettersecret",
+  secret,
   touchAfter: 24 * 60 * 60, // time period in seconds
 });
 
@@ -67,7 +69,7 @@ store.on("error", function (e) {
 const sessionConfig = {
   store,
   name: "session",
-  secret: "thisshouldbeabettersecret",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
