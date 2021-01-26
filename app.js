84885807bf57d555ad2707120d531564c12d46d1
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-26 15:04:43 +0300

    setting up mongo atlas.

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ const helmet = require("helmet");
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
+// const dbUrl = process.env.DB_URL;
 
+// mongodb://localhost:27017/yelp-camp
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useCreateIndex: true,
