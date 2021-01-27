5eb592e41fdc105f5bb5641f2fcebdfdd2f17b41
Cagatay Soyer <cagataysoyer@gmail.com>
2021-01-27 16:08:08 +0300

    db url

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const reviewRoutes = require("./routes/reviews");
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
-// process.env.DB_URL ||
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+
 // mongodb://localhost:27017/yelp-camp
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
